bc9463f2eb81f33896434c538241a0ea934543f0
crystalmenthol <chalermchai_v@kkumail.com>
2021-10-15 09:27:14 +0700

    Add routing, add landing and profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { LandingComponent } from './components/landing/landing.component';
 
+const appRoutes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'profile', component: ProfileComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,51 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from 'src/app/models/employeeModel';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {
-  employees: Employee[] = [
-    {
-      id: 1,
-      name: 'wut',
-      task: 'code',
-      workingHour: '8'
-    },
-    {
-      id: 2,
-      name: 'john',
-      task: 'test',
-      workingHour: '8'
-    },
-    {
-      id: 3,
-      name: 'jay',
-      task: 'ux',
-      workingHour: '8'
-    },
-    {
-      id: 4,
-      name: 'em',
-      task: 'ui',
-      workingHour: '8'
-    },
-    {
-      id: 5,
-      name: 'best',
-      task: 'code',
-      workingHour: '8'
-    },
-  ]
-  showEmp: Employee[] = this.employees;
+export class LandingComponent implements OnInit {
 
   constructor() { }
 
-  check(value: string): void {
-    value? this.showEmp = this.employees.filter(employee => employee.name == value) : this.showEmp = this.employees;
-    console.log(this.showEmp);
+  ngOnInit(): void {
   }
-  
+
 }
